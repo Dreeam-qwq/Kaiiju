0430579862dfb0418e63ba9368a732f5132b3f49
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-01-12 05:00:04 -0500

    1.20.4 Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("io.papermc.paperweight.patcher") version "1.5.9"
+    id("io.papermc.paperweight.patcher") version "1.5.11"
 }
 
 allprojects {
@@ -29,7 +29,7 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
     decompiler("net.minecraftforge:forgeflower:2.0.629.0")
     paperclip("io.papermc:paperclip:3.0.3")
 }
